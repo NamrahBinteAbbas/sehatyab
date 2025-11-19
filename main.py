9754b187196adea80be802d28a223c5b5c2d904e
AmanTheDrummer <amzalankhan@gmail.com>
2025-11-19 11:53:04 +0500

    proper file structure and backend

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from website import create_app
-app = create_app()
-if __name__ == '__main__':
-    app.run(debug = True) #automatically reruns the webserver when you make changes to your python code
-@auth.route('/admin.html')
+from flask import Flask, Blueprint, render_template
+from flask_cors import CORS
+from websites.views import views
+from websites.auth import auth
+
+app = Flask(__name__)
+CORS(app)
+app.config['SECRET_KEY'] = 'namrahsapp'
+
+# register blueprints
+app.register_blueprint(auth)   # for '/' route
+app.register_blueprint(views)  # for '/dashboard' route
+
+app.route('/admin.html')
 def admin():
     return render_template("admin.html")
+
+if __name__ == "__main__":
+    print("[INFO] Starting Flask app...")
+    app.run(debug=True)
